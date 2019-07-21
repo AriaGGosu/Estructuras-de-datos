a67ee70ff5d471db99a6f06b2e7b63e6df5e18a4
AriaGGosu <hector.vicencio.reyes@gmail.com>
2019-07-21 17:25:35 -0400

    working in a clock

---
+++ tareas/time.js
@@ -0,0 +1,18 @@
+let date = new Date();
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log('dia ' + date.getDay());
+// el dia comienza desde el 0 como si fuera un arreglo y desde el domingo
+// Sunday	= 0
+// Monday = 1
+// Tuesday = 2
+// Wednesday = 3
+// Thursday	= 4
+// Friday = 5
+// Saturday = 6
+
+console.log(date.getDate());
